8ec4452b55760be537de9f540c12d0a4e35673de
Hélder Gonçalves <helder.goncalves@outsystems.com>
2021-07-05 18:26:25 +0100

    Give more responsibility to WebView

--- ReactViewControl/ExtendedWebView.cs
+++ ReactViewControl/ExtendedWebView.cs
@@ -9,13 +9,13 @@ namespace ReactViewControl {
 
         protected override bool OnSetFocus(bool isSystemEvent) {
             // prevent stealing focus on navigation, only allow when system explicitly sends a focus signal
-            return !isSystemEvent;
+            return !isSystemEvent ? true : base.OnSetFocus(isSystemEvent);
         }
 
         public new Task<T> EvaluateScriptFunctionWithSerializedParams<T>(string functionName, params object[] args) {
             return base.EvaluateScriptFunctionWithSerializedParams<T>(functionName, args);
         }
-    
+
         public new void ExecuteScriptFunctionWithSerializedParams(string functionName, params object[] args) {
             base.ExecuteScriptFunctionWithSerializedParams(functionName, args);
         }
