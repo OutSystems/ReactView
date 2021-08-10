64c7ac49f9ff6a9ae58b19c798b154f8551c673c
Júlio Lopes <61831687+os-jsplopes@users.noreply.github.com>
2021-08-10 18:04:19 +0100

    RMAC-4091: Update rule for loading component images (#54)
    * Update rule for loading component images
    
    * revert specific scope reference on file load rule

--- ViewGenerator/tools/webpack/Rules/Files.ts
+++ ViewGenerator/tools/webpack/Rules/Files.ts
@@ -23,53 +23,14 @@ const getResourcesRuleSet = (assemblyName?: string, pluginsBase? : string): Rule
                         //
                         // Context represents the path of the project being built by webpack, e.g. "C:\Git\Path\to\Project\"
                         //
-                        const PackagesFolder = "/packages/";
-                        const isOutSystemsPackagesWithOldNaming = (url) => {
-                            // means that the package name is (example) "web_feedback-message" or "web_image" instead of "web-feedback-message" or "web-image"
-                            const OutSystemsPackagesWithOldNaming = [
-                                "/carousel/",
-                                "/checkbox/",
-                                "/collapsible/",
-                                "/common/",
-                                "/editable-label/",
-                                "/feedback-message/",
-                                "/image/",
-                                "/input/",
-                                "/loading-legacy/",
-                                "/loading-wheel/",
-                                "/navigation-bar/",
-                                "/optioncards/",
-                                "/overlay/",
-                                "/progressbar/",
-                                "/radio-button/",
-                                "/slider/",
-                                "/splitter/",
-                                "/tabs/",
-                                "/theme/",
-                                "/theme-provider/",
-                                "/toggle/",
-                            ];
-
-                            for(var i = 0; i < OutSystemsPackagesWithOldNaming.length; ++i) {
-                                if(url.indexOf(OutSystemsPackagesWithOldNaming[i]) > 0) {
-                                    return true;
-                                }
-                            }
-                            return false;
-                        }
                         const buildUrl = (url: string, resourceBase: string): string => {
-                            const PackagesFolder = "/packages/";
-                            let isOutSystemsWebPackage = false;
+
                             let idx: number = url.indexOf(`/${resourceBase}/`);
+
                             if (idx < 0 && pluginsBase) {
                                 idx = url.indexOf(`/${pluginsBase}/`);
                             }
-                            if (idx < 0 && url.indexOf(PackagesFolder) > 0) {
-                                // OutSystems web components packages
-                                idx = url.indexOf(`${url}`);
-                                isOutSystemsWebPackage = true;
-                            }
-
+                            
                             // relative paths starting with ".." are replaced by "_"
                             if (url.startsWith("_")) {
                                 if (idx < 0) {
@@ -77,13 +38,7 @@ const getResourcesRuleSet = (assemblyName?: string, pluginsBase? : string): Rule
                                     throw new Error("VG001: Resource not found: using a resource from another namespace without an absolute URL.");
                                 }
 
-                                // URL (argument) is a relative path and contains the resource base path or the plugin assembly in its content
-                                if(isOutSystemsWebPackage) {
-                                    let packg = url.substring(url.indexOf(PackagesFolder) + PackagesFolder.length);
-                                    return `/${pluginsBase}/node_modules/@outsystems/web${(isOutSystemsPackagesWithOldNaming(url)) ? "_" : "-"}${packg}`;
-                                } else {
-                                    return url.substring(idx);
-                                }
+                                return url.substring(idx);
                             }
 
                             if (idx >= 0) {
