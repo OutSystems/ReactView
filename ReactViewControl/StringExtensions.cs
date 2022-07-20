94260d7e05767a36df87b3182e4752ba32a63fb0
Miguel Marques <miguel.angelo.marques@outsystems.com>
2022-07-20 17:55:56 +0100

    fix indentation

--- ReactViewControl/StringExtensions.cs
+++ ReactViewControl/StringExtensions.cs
@@ -3,11 +3,11 @@ using System.Globalization;
 
 namespace ReactViewControl {
 
-      internal static class StringExtensions {
-        public static bool ContainsOrdinal(this string str, string value) => str.IndexOf(value, 0, StringComparison.Ordinal) > -1;
+    internal static class StringExtensions {
+        public static bool OrdinalContains(this string str, string value) => str.IndexOf(value, 0, StringComparison.Ordinal) > -1;
 
-        public static bool StartsWithOrdinal(this string str, string value) => str.StartsWith(value, StringComparison.Ordinal);
+        public static bool OrdinalStartsWith(this string str, string value) => str.StartsWith(value, StringComparison.Ordinal);
 
-        public static int IndexOfOrdinal(this string str, string value) => str.IndexOf(value, 0, StringComparison.Ordinal);
+        public static int OrdinalIndexOf(this string str, string value) => str.IndexOf(value, 0, StringComparison.Ordinal);
     }
 }
