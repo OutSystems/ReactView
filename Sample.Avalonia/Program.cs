3853077f5a20527efb1da21c50f177b0502e8d42
Luiz Santos <luiz.santos@outsystems.com>
2023-01-12 17:56:58 +0000

    Fix application startup on windows

--- Sample.Avalonia/Program.cs
+++ Sample.Avalonia/Program.cs
@@ -5,6 +5,8 @@ namespace Sample.Avalonia {
         static void Main(string[] args) {
             AppBuilder.Configure<App>()
                       .UsePlatformDetect()
+                      .UsePlatformDetect()
+                      .With(new Win32PlatformOptions { UseWindowsUIComposition = false })
                       .StartWithClassicDesktopLifetime(args);
         }
     }
