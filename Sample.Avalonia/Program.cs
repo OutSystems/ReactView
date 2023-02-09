0e46294b6f699494c259bc2b3f34f22dda44d40f
Luiz Alberto de Castro Santos <luiz.santos@outsystems.com>
2023-02-09 15:16:43 +0000

    RDOIAT-710: Upgrade ReactView to bring new CEF/CefGlue (#119)
    * Upgrade webview to bring new CEF/CefGlue
    
    * Upgrade webview control;
    
    * Upgrade avalonia version
    
    * Fix application startup on windows
    
    * Remove duplicated line
    
    * Upgrade to .net 6.0
    
    * Move to .net 6
    
    * Update webview version
    
    * Revert useless changes
    
    * Update webview version
    
    * RICT-5059 :: adding missing include assets
    
    * RICT-5059 :: changing avalonia ref
    
    ---------
    
    Co-authored-by: José Santos <jose.dos.santos@outsystems.com>

--- Sample.Avalonia/Program.cs
+++ Sample.Avalonia/Program.cs
@@ -5,6 +5,7 @@ namespace Sample.Avalonia {
         static void Main(string[] args) {
             AppBuilder.Configure<App>()
                       .UsePlatformDetect()
+					  .With(new Win32PlatformOptions { UseWindowsUIComposition = false })
                       .StartWithClassicDesktopLifetime(args);
         }
     }
